f173f1822b977e4bb10fe5f0364ff55bd4aa5cac
Scott <70402467+SWilli93@users.noreply.github.com>
2024-05-02 23:13:34 -0400

    mouse event logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { count } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'Interactive-World-Map-Project';
+  focusCountryName: any;
+  countryResponseInfo: any;
+  
+
+  constructor(private http: HttpClient) {}
+
+  @ViewChild('interactiveWorldMap', { static: false}) interactiveWorldMap!: ElementRef<SVGSVGElement>;
+
+  ngAfterViewInit() {
+    this.svgCountryEventListeners();
+  }
+
+  svgCountryEventListeners() {
+    const svgElements = this.interactiveWorldMap.nativeElement;
+    const svgCountries = svgElements.querySelectorAll('path');
+    svgCountries.forEach((country => {
+      const countryId = country.getAttribute('id')
+
+      if(!countryId || countryId === 'null') { return; }
+
+      country.addEventListener('mouseover', (event) => this.onMouseOver(event));
+      country.addEventListener('mouseout', (event) => this.onMouseOut(event));
+      country.addEventListener('click', (event) => this.onClick(event));
+    }))
+  }
+
+  onMouseOver(event: MouseEvent) {
+    const focus = event.target as SVGPathElement;
+    const countryId = focus.getAttribute('id')
+    const url = `https://api.worldbank.org/V2/country/${countryId}?format=json`;
+
+    this.http.get<any[]>(url).subscribe((Response) => {if (Response) {this.focusCountryName = Response[1][0].name}});
+    console.log(Response)
+  }
+
+  onMouseOut(event: MouseEvent) {
+    this.focusCountryName = '';
+   }
+
+  onClick(event: MouseEvent) {
+    const focus = event.target as SVGPathElement;
+    const countryId = focus.getAttribute('id')
+    const url = `https://api.worldbank.org/V2/country/${countryId}?format=json`;
+
+    this.http.get<any[]>(url).subscribe((Response) => {if (Response != null) {this.countryResponseInfo = Response[1][0]}});
+  }
 }
+
